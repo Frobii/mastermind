5a18d746d7294f6d6790ea68f0f64ce293b3cf59
Frobi <frobi.webdev@outlook.com>
2022-08-05 19:04:56 +0930

    create-method-for-computer-rng

--- mastermind.rb
+++ mastermind.rb
@@ -1,1 +1,21 @@
+class Computer
+attr_reader :randomPicks
 
+    def pick_numbers()
+        @randomPicks = []
+        i = 0
+        until i == 6
+            @randomPicks.push(rand(1..6))
+            i += 1
+        end
+    end
+            
+end
+
+
+computer = Computer.new
+
+computer.pick_numbers()
+
+
+p computer.randomPicks
