c8efc3af0311aefc46a57c4db503a4c7c45f1ed4
Frobi <frobi.webdev@outlook.com>
2022-08-07 17:08:31 +0930

    create-number-to-color-module

--- mastermind.rb
+++ mastermind.rb
@@ -1,15 +1,40 @@
+require "colorize" # a ruby gem which allows color to be output in the terminal
+
+module Color_Numbers
+    def add_color(array)
+        array.map! { |number|
+            if number == "1"
+                number.red + " " # add spaces to make the output easier to read
+            elsif number == "2"
+                number.yellow + " "
+            elsif number == "3"
+                number.blue + " "
+            elsif number == "4"
+                number.green + " "
+            elsif number == "5"
+                number.cyan + " "
+            elsif number == "6"
+                number.magenta + " "
+            end
+        }
+    end
+end
+
 class Computer
-attr_reader :randomPicks
+    include Color_Numbers
+
+    attr_reader :randomPicks
+    attr_writer :randomPicks
 
     def pick_numbers()
         @randomPicks = []
         i = 0
         until i == 6
-            @randomPicks.push(rand(1..6))
+            @randomPicks.push(rand(1..6).to_s)
             i += 1
         end
     end
-            
+
 end
 
 
@@ -17,5 +42,6 @@ computer = Computer.new
 
 computer.pick_numbers()
 
+roundPicks = computer.add_color(computer.randomPicks)
 
-p computer.randomPicks
+puts roundPicks.join("")
