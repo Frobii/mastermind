efa67a5b6df9d33898ddb3387111868afb77beb1
Frobi <frobi.webdev@outlook.com>
2022-08-11 15:00:12 +0930

    use-a-local-variable-for-guess

--- mastermind.rb
+++ mastermind.rb
@@ -21,12 +21,14 @@ module Color_Numbers
 end
 
 module Play_Game
+    attr_reader :guess
+
     def play_round(solution)
         puts "Enter four numbers between 1-6:"
-        guess = gets.chomp.to_s.split("")
+        @guess = gets.chomp.to_s.split("")
         puts "\n"
 
-        if guess.join.to_i >= 7000 || guess.join.to_i < 1000
+        if @guess.join.to_i >= 7000 || @guess.join.to_i < 1000
             puts "\n"
             puts "Invalid Input".red
             puts "\n"
@@ -34,24 +36,25 @@ module Play_Game
             return
         end
 
-        clues = "Clues: "
+        clues = ""
         i = 0
         
         until i == 4
-            if solution[i] == guess[i] # checks for the right number in the right position
+            if solution[i] == @guess[i] # checks for the right number in the right position
                 clues += "● "
-            elsif solution[0] == guess[i] && guess[0] != solution[0]  # checks for the right number in the wrong position
+            elsif solution[0] == @guess[i] && @guess[0] != solution[0]  # checks for the right number in the wrong position
                 clues += "○ "
-            elsif solution[1] == guess[i] && guess[1] != solution[1]
+            elsif solution[1] == @guess[i] && @guess[1] != solution[1]
                 clues += "○ "
-            elsif solution[2] == guess[i] && guess[2] != solution[2]
+            elsif solution[2] == @guess[i] && @guess[2] != solution[2]
                 clues += "○ "
-            elsif solution[3] == guess[i] && guess[3] != solution[3]
+            elsif solution[3] == @guess[i] && @guess[3] != solution[3]
                 clues += "○ "
             end
+
             i += 1
 
-            if i == 4 # on the last iteration, print the result of the guess
+            if i == 4 # on the last iteration, print the result of the @guess
                 puts clues
             end
         end
@@ -78,6 +81,7 @@ end
 
 class Player
     include Play_Game
+    include Color_Numbers
 end
 
 
@@ -92,10 +96,13 @@ puts computer.add_color(computer.randomPicks).join("")
 
 player1 = Player.new
 
-i = 0
+i = 1
 
-until i == 12
-    player1.play_round(computerSolution)
+until i == 13
+    puts "Guess #{i} / 12".green
+    roundResult = player1.play_round(computerSolution)
+    puts "\n"
+    puts player1.add_color(player1.guess).join("")
     puts "\n"
     i += 1
 end
